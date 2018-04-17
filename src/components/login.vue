27c706a6e095f50d080911d4c18625e24ed3d9ec
zmlovelin <656030719@qq.com>
2018-04-17 23:18:04 +0800

    搭建项目

---
+++ src/components/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+   {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: '授权'
+      }
+    }
+}
+</script>
+<style scoped>
+
+</style>
