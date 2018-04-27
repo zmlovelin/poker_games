9fad36b394102edee0ff70053258d73dcda882c3
lzm12345678 <656030719@qq.com>
2018-04-27 15:07:56 +0800

    model ok

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,9 @@ export default {
 <style>
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        /*text-align: center;*/
         color: #2c3e50;
         margin: 0;
         padding: 0;
+        height: 100%;
     }
 </style>
